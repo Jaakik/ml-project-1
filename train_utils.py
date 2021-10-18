7c7d5edcf542cb6f73d9cb93b3b02240b9e37bc2
YassineKhalfi <33032937+YassineKhalfi@users.noreply.github.com>
2021-10-18 18:15:56 +0200

    Compute loss function for least square and ridge

--- train_utils.py
+++ train_utils.py
@@ -37,3 +37,21 @@ def sigmoid(t):
     apply sigmoid function on t.
     """
     return 1.0 / (1 + np.exp(-t))
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
+
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y - tx.dot(w)
+    return calculate_mse(e)
